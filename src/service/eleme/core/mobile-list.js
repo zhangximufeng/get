8b8d1b9486d4b9a9093adbc3c8558f71511b8559
zhuweiyou <i@zhuweiyou.com>
2018-06-19 15:38:34 +0800

    去重收集到的列表

--- src/service/eleme/core/mobile-list.js
+++ src/service/eleme/core/mobile-list.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const {white = [], black = []} = require('./mobile-list.json');
+const uniq = require('lodash/uniq');
+const Random = require('../../../util/random');
+let {white = [], black = []} = require('./mobile-list.json');
+// 为了收集时的效率，没有判断重复。在下次读取的时候去重一下
+white = uniq(white);
+black = uniq(black);
 
 // 定时存一下
 setInterval(() => {
@@ -10,14 +15,18 @@ setInterval(() => {
 module.exports = {
   // 加入白名单，用于以后快速随机
   addWhite(value) {
-    if (!white.includes(value)) {
-      white.push(value);
-    }
+    white.push(value);
   },
   // 加入黑名单，以后随机不用它
   addBlack(value) {
-    if (!black.includes(value)) {
-      black.push(value);
-    }
+    black.push(value);
+  },
+  // TODO: 随机一个可用的手机号码，暂时没用上，先收集足够的量
+  getWhite(exclude) {
+    let value;
+    do {
+      value = Random.array(white);
+    } while (value === exclude);
+    return value;
   }
 };
