867364b5630c24b34c031111fde197b36c77994f
zhuweiyou <i@zhuweiyou.com>
2018-06-28 16:43:33 +0800

    美团链接支持 http

--- src/service/meituan/get-hongbao.js
+++ src/service/meituan/get-hongbao.js
@@ -20,11 +20,7 @@ module.exports = async (req, res) => {
     return response(2, '请填写 11 位手机号码');
   }
 
-  if (
-    String(url)
-      .trim()
-      .indexOf('https://activity.waimai.meituan.com/') !== 0
-  ) {
+  if (/^https?:\/\/activity.waimai.meituan.com\//i.test(url)) {
     return response(3, '美团红包链接不正确');
   }
 
